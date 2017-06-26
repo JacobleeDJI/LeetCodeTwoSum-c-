1d386bbfbd6bf18cfc4182655ef63cf833937c15
Jacoblee <jacobleeloveru@gmail.com>
2017-06-26 11:14:10 +0800

    Initial Commit

---
+++ leeCode_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  leeCode_1
+//
+//  Created by Jacoblee on 2017/6/26.
+//  Copyright © 2017年 Jacoblee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
